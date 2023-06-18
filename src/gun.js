4a7ceb6a5c6255cef8714d2366cffdd26c935cf8
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-18 22:17:25 +0800

    Update gun.js

--- src/gun.js
+++ src/gun.js
@@ -1,4 +1,4 @@
-import { GUN_POSITION, POSTURE } from 'constant';
+import { GUN_CATEGORIES, GUN_POSITION, POSTURE } from 'constant';
 import { isPointNotEmpty } from 'utils';
 
 /**
@@ -20,4 +20,30 @@ export function getGunPosition() {
   }
 
   return null;
+}
+
+export function getCurrentGunName() {
+  const gunPosition = getGunPosition();
+
+  const sim = 0.85;
+  const setCol = 4;
+  const setRow = 4;
+  const selectCol = gunPosition === GUN_POSITION.left ? 2 : 3;
+  const selectRow = 4;
+
+  let currentGunName = null;
+
+  Object.keys(GUN_CATEGORIES).some((gunName) => {
+    const point = mapi.findimage(gunName, sim, setCol, setRow, selectCol, selectRow);
+
+    const isExist = isPointNotEmpty(point);
+
+    if (isExist) {
+      currentGunName = gunName;
+    }
+
+    return isExist;
+  })
+
+  return currentGunName;
 }
