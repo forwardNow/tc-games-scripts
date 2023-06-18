e94da3d34319eb73204d085dbcec72a53f3d8320
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-18 21:42:49 +0800

    Update mapi.d.ts

--- types/mapi.d.ts
+++ types/mapi.d.ts
@@ -12,4 +12,15 @@ declare namespace mapi {
   function tip(info: string, closeTime?: number): void;
 
   function findcolor(color: string, poslist: string): point;
+
+  /**
+   * 简单找图，返回当前图标在屏幕的坐标点
+   * @param imgName 图片的名称
+   * @param sim 相似度
+   * @param setCol 屏幕分几列
+   * @param setRow 屏幕分几行
+   * @param selectCol 取第几列
+   * @param selectRow 取第几行
+   */
+  function findimage(imgName: string, sim: number, setCol: number, setRow: number, selectCol: number, selectRow: number): point;
 }
