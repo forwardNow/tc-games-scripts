e1a4cb8eceb46d494602b88b3dc09239d702c1a8
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-18 21:26:38 +0800

    Update posture.js

--- src/posture.js
+++ src/posture.js
@@ -1,4 +1,5 @@
 import { POSTURE } from 'constant';
+import { isPointNotEmpty } from 'utils';
 
 /**
  * 获取姿势
@@ -8,24 +9,16 @@ import { POSTURE } from 'constant';
 export function getPosture() {
   const squatPoint = mapi.findcolor('255:255:241:119', '1305:653+1307:658+1307:665+1312:672+1313:681+1313:684');
 
-  if (isNotEmpty(squatPoint)) {
+  if (isPointNotEmpty(squatPoint)) {
     return POSTURE.squat;
   }
 
   const prostrationPoint = mapi.findcolor('255:253:237:119', '1421:639+1426:644+1437:649+1445:652');
 
-  if (isNotEmpty(prostrationPoint)) {
+  if (isPointNotEmpty(prostrationPoint)) {
     return POSTURE.prostration;
   }
 
   return POSTURE.stand;
 }
 
-/**
- *
- * @param point { mapi.point }
- * @returns {boolean}
- */
-function isNotEmpty(point) {
-  return point.X !== 0 || point.Y !== 0;
-}
