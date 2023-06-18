699c1e83ade79b212842e6296f2388d05479351d
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-18 22:20:06 +0800

    Update gun.js

--- src/gun.js
+++ src/gun.js
@@ -3,8 +3,8 @@ import { isPointNotEmpty } from 'utils';
 
 /**
  * 获取当前持枪的位置
- * 变量 持左枪=多点找色 255:255:221:0 613:613
- * 变量 持右枪=多点找色 255:254:220:1 786:614
+ *
+ * @return { string | null }
  */
 export function getGunPosition() {
   const leftPoint = mapi.findcolor('255:255:221:0', '613:613');
