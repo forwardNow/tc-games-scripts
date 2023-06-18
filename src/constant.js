460b7b94c62a4b23c4ccd2a1b393f58785031d40
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-18 22:17:27 +0800

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -8,10 +8,45 @@ export const POSTURE = {
   prostration: 'prostration'
 };
 
+/** 持枪位置 */
 export const GUN_POSITION = {
+  /** 左边枪 */
   left: 'left',
+  /** 右边枪 */
   right: 'right',
 }
+
+/** 枪的类型 */
+export const GUN_CATEGORIES = {
+  M4: 'M4',
+  SCARL: 'SCARL',
+  DP28: 'DP28',
+
+  UMP45: 'UMP45',
+  YENIU: 'YENIU',
+  UZI: 'UZI',
+  VECTOR: 'VECTOR',
+  TANGMUXUN: 'TANGMUXUN', // 汤姆逊枪
+
+  AKM: 'AKM',
+
+  ACVAL: 'ACVAL',
+
+  M249: 'M249',
+
+  M762: 'M762',
+
+  AUG: 'AUG',
+  GROZA: 'GROZA',
+  MG3: 'MG3',
+  P90: 'P90',
+
+  MIGUAN: 'MIGUAN', // 蜜罐
+};
+
+export const GUN_PRESS_CONFIGS = {
+
+};
 
 /** 准镜状态 */
 export const MIRROR_STATUS = {
