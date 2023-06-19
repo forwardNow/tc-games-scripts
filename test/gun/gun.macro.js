02b630b7b28c433c145f85bffb77e3950a8000ff
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-19 16:14:35 +0800

    Create gun.macro.js

---
+++ test/gun/gun.macro.js
@@ -0,0 +1,24 @@
+/**
+ * 在小键盘 num2 上绑定
+ */
+(function test_getPosture() {
+  import('gun')
+    .then((module) =>{
+      const gunPosition = module.getGunPosition();
+
+      mapi.tip(gunPosition, 1);
+    });
+}());
+
+
+/**
+ * 在小键盘 num3 上绑定
+ */
+(function test_getPosture() {
+  import('gun')
+    .then((module) =>{
+      const currentGunName = module.getCurrentGunName();
+
+      mapi.tip(currentGunName, 1);
+    });
+}());
