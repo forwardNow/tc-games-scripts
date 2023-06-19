829c84cc3f09599672d78aee4358da667a00dc2a
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-19 11:31:30 +0800

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,20 +1,4 @@
-/** 姿势 */
-export const POSTURE = {
-  /** 站 */
-  stand: 'stand',
-  /** 蹲 */
-  squat: 'squat',
-  /** 趴 */
-  prostration: 'prostration'
-};
 
-/** 持枪位置 */
-export const GUN_POSITION = {
-  /** 左边枪 */
-  left: 'left',
-  /** 右边枪 */
-  right: 'right',
-}
 
 /** 枪的类型 */
 export const GUN_CATEGORIES = {
