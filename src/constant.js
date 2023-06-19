7dc4d06635b32272507d0e1067d64d9525d2aa6f
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-19 11:33:11 +0800

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,37 +1,5 @@
 
 
-/** 枪的类型 */
-export const GUN_CATEGORIES = {
-  M4: 'M4',
-  SCARL: 'SCARL',
-  DP28: 'DP28',
-
-  UMP45: 'UMP45',
-  YENIU: 'YENIU',
-  UZI: 'UZI',
-  VECTOR: 'VECTOR',
-  TANGMUXUN: 'TANGMUXUN', // 汤姆逊枪
-
-  AKM: 'AKM',
-
-  ACVAL: 'ACVAL',
-
-  M249: 'M249',
-
-  M762: 'M762',
-
-  AUG: 'AUG',
-  GROZA: 'GROZA',
-  MG3: 'MG3',
-  P90: 'P90',
-
-  MIGUAN: 'MIGUAN', // 蜜罐
-};
-
-export const GUN_PRESS_CONFIGS = {
-
-};
-
 /** 准镜状态 */
 export const MIRROR_STATUS = {
   /** 开镜 */
