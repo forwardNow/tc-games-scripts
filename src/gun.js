1b01ffa2b21206af78de55416be12ef57b196deb
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-19 12:28:30 +0800

    Update gun.js

--- src/gun.js
+++ src/gun.js
@@ -8,6 +8,12 @@ export const GUN_POSITION = {
   RIGHT: 'right',
 }
 
+/* 持枪位置-颜色点 */
+const GUN_POSITION_COLOR_POINT = {
+  [GUN_POSITION.LEFT]: ['255:255:221:0', '613:613'],
+  [GUN_POSITION.RIGHT]: ['255:254:220:1', '786:614'],
+};
+
 /** 枪的类型 */
 export const GUN_CATEGORIES = {
   M4: 'M4',
@@ -42,13 +48,13 @@ export const GUN_CATEGORIES = {
  * @return { string | null }
  */
 export function getGunPosition() {
-  const leftPoint = mapi.findcolor('255:255:221:0', '613:613');
+  const leftPoint = mapi.findcolor(...GUN_POSITION_COLOR_POINT[GUN_POSITION.LEFT]);
 
   if (isPointNotEmpty(leftPoint)) {
     return GUN_POSITION.LEFT;
   }
 
-  const rightPoint = mapi.findcolor('255:254:220:1', '786:614');
+  const rightPoint = mapi.findcolor(...GUN_POSITION_COLOR_POINT[GUN_POSITION.RIGHT]);
 
   if (isPointNotEmpty(rightPoint)) {
     return GUN_POSITION.RIGHT;
@@ -57,6 +63,23 @@ export function getGunPosition() {
   return null;
 }
 
+/* 持枪位置-图片区域 */
+const GUN_POSITION_IMAGE_AREA = {
+  [GUN_POSITION.LEFT]: {
+    SIMILARITY: 0.85,
+    TOTAL_COLUMNS: 4,
+    TOTAL_ROWS: 4,
+    COLUMN_INDEX: 2, // 第 2 列
+    ROW_INDEX: 4,
+  },
+  [GUN_POSITION.RIGHT]: {
+    SIMILARITY: 0.85,
+    TOTAL_COLUMNS: 4,
+    TOTAL_ROWS: 4,
+    COLUMN_INDEX: 3, // 第 3 列
+    ROW_INDEX: 4,
+  },
+}
 
 /**
  * 获取当前枪械的名称
@@ -66,16 +89,18 @@ export function getGunPosition() {
 export function getCurrentGunName() {
   const gunPosition = getGunPosition();
 
-  const sim = 0.85;
-  const setCol = 4;
-  const setRow = 4;
-  const selectCol = gunPosition === GUN_POSITION.LEFT ? 2 : 3;
-  const selectRow = 4;
+  const {
+    SIMILARITY,
+    TOTAL_COLUMNS,
+    TOTAL_ROWS,
+    COLUMN_INDEX,
+    ROW_INDEX,
+  } = GUN_POSITION_IMAGE_AREA[gunPosition];
 
   let currentGunName = null;
 
   Object.keys(GUN_CATEGORIES).some((gunName) => {
-    const point = mapi.findimage(gunName, sim, setCol, setRow, selectCol, selectRow);
+    const point = mapi.findimage(gunName, SIMILARITY, TOTAL_COLUMNS, TOTAL_ROWS, COLUMN_INDEX, ROW_INDEX);
 
     const isExist = isPointNotEmpty(point);
 
