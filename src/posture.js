8bd615abba547a4fb4882d89f809503acda5cac6
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-19 11:56:36 +0800

    Update posture.js

--- src/posture.js
+++ src/posture.js
@@ -11,7 +11,7 @@ export const POSTURE_CATEGORIES = {
 };
 
 /** 姿势的点的颜色 */
-export const POSTURE_POINT_COLOR = {
+const COLOR_POINT = {
   [POSTURE_CATEGORIES.SQUAT]: ['255:255:241:119', '1305:653+1307:658+1307:665+1312:672+1313:681+1313:684'],
   [POSTURE_CATEGORIES.PROSTRATION]: ['255:253:237:119', '1421:639+1426:644+1437:649+1445:652'],
 }
@@ -22,13 +22,13 @@ export const POSTURE_POINT_COLOR = {
  * @return {string}
  */
 export function getPosture() {
-  const squatPoint = mapi.findcolor(...POSTURE_POINT_COLOR[POSTURE_CATEGORIES.SQUAT]);
+  const squatPoint = mapi.findcolor(...COLOR_POINT[POSTURE_CATEGORIES.SQUAT]);
 
   if (isPointNotEmpty(squatPoint)) {
     return POSTURE_CATEGORIES.SQUAT;
   }
 
-  const prostrationPoint = mapi.findcolor(...POSTURE_POINT_COLOR[POSTURE_CATEGORIES.PROSTRATION]);
+  const prostrationPoint = mapi.findcolor(...COLOR_POINT[POSTURE_CATEGORIES.PROSTRATION]);
 
   if (isPointNotEmpty(prostrationPoint)) {
     return POSTURE_CATEGORIES.PROSTRATION;
