532836984d71c54ca62e7aeb47302792ee83c627
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-23 21:38:24 +0800

    Update variable.js

--- src/variable.js
+++ src/variable.js
@@ -11,17 +11,20 @@ export default {
   get isTipEnabled() {
     return mapi.getglobalmap(KEYS.IS_TIP_ENABLED);
   },
+
   set isTipEnabled(isTipEnabled) {
     mapi.setglobalmap(KEYS.IS_TIP_ENABLED, isTipEnabled);
   },
 
-  getX6SightOfGun(gun) {
+  // 获取调整过 6 倍镜倍率的枪的准镜
+  getX6SightOfAdjustedGun(gun) {
     const adjustedX6SightsGuns = mapi.getglobalmap(KEYS.ADJUSTED_X6_SIGHTS_GUNS) || {};
 
     return adjustedX6SightsGuns[gun];
   },
 
-  setX6SightOfGun(gun, mirror) {
+  // 设置调整过 6 倍镜倍率的枪及准镜
+  setX6SightOfAdjustedGun(gun, mirror) {
     const adjustedX6SightsGuns = mapi.getglobalmap(KEYS.ADJUSTED_X6_SIGHTS_GUNS) || {};
 
     adjustedX6SightsGuns[gun] = mirror;
