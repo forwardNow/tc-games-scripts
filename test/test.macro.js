dd7cfdf10e33d927c910c83b85b3a2d96e288d6e
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-23 11:59:38 +0800

    Update test.macro.js

--- test/test.macro.js
+++ test/test.macro.js
@@ -5,3 +5,20 @@
     mapi.tip(`Posture.getCurrentPosture(): ${ result }`);
   });
 } ());
+
+// num2
+(function testGun() {
+  import('gun').then(({ default: Gun }) => {
+    const result = Gun.getCurrentGun();
+    mapi.tip(`Gun.getCurrentGun(): ${ result }`);
+  });
+} ());
+
+// num3
+(function testMirror() {
+  import('mirror').then(({ default: Mirror }) => {
+    const isOpen = Mirror.isOpen();
+    const getCurrentMirror = Mirror.getCurrentMirror();
+    mapi.tip(`Mirror.isOpen(): ${ isOpen }。Mirror.getCurrentMirror(): ${ getCurrentMirror }。`);
+  });
+} ());
