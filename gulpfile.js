0e56558e444dbb1c364c54ae16e1cf8e21f0b6a2
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-23 10:48:50 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
+const { resolve } = require('path');
 const { src, dest } = require('gulp');
+
+// C:\Users\wuqinfei
+const USER_HOME = process.env.HOME || process.env.USERPROFILE;
+
+const scriptDirPath = resolve(USER_HOME, 'AppData\\Roaming\\Sigma-RT\\TC Games\\resource\\Scripts')
 
 exports.default = function() {
   // 读取文件
   return src('src/*.js')
     // 写到 output 目录
-    .pipe(dest('C:\\Users\\abc\\AppData\\Roaming\\Sigma-RT\\TC Games\\resource\\Scripts'));
+    .pipe(dest(scriptDirPath));
 }
