212901027aae4ef5bd4e37a59426d022d3f49fba
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-03 20:55:11 +0800

    Update clean.js

--- builds/clean.js
+++ builds/clean.js
@@ -1,18 +1,8 @@
-const { src } = require('gulp');
-const clean = require('gulp-clean');
+const { rmSync } = require('fs');
 
-module.exports = function(cwd) {
-  return function() {
-    return (
-      src(
-        './**/*', // 所有文件及目录
-        {
-          cwd,          // 工作目录
-          read: false,  // 不需要读取文件内容
-          dot: true,    // 以 . 打头的文件也包含
-        }
-      )
-        .pipe(clean())
-    );
+module.exports = function(dir) {
+  return function(next) {
+    rmSync(dir, { recursive: true });
+    next();
   };
 }
