44a4479832df52d94d7e56999c94e84eeef59a62
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-03 20:36:33 +0800

    update

--- builds/buildScripts.js
+++ builds/buildScripts.js
@@ -1,9 +1,13 @@
 const { src, dest, series } = require('gulp');
+const replace = require('gulp-replace');
 const clean = require('./clean')
 const { CTX_PATH, TCG_SCRIPTS_DIR } = require('./utils');
 
+const DEVICE = process.env.DEVICE;
+
 function build() {
   return src(`src/scripts/*.js`, { cwd: CTX_PATH })
+    .pipe(replace(/\/\*\s*replace\s*start\s*\*\/\s*\w+\s*\/\*\s*replace end\s*\*\//, DEVICE))
     .pipe(dest(TCG_SCRIPTS_DIR));
 }
 
