8fc5f8f3364cbec34c9a3d0cc64cb6d41e524806
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-08 18:49:33 +0800

    Update missile.js

--- src/scripts/missile.js
+++ src/scripts/missile.js
@@ -4,11 +4,25 @@ import Utils from 'utils';
  * 投掷物类别
  */
 const CATEGORIES = {
+  // 破片手榴弹
+  FRAGMENT_BOMB: 'FRAGMENT_BOMB',
 
+  // 烟雾弹
+  SMOKE_BOMB: 'SMOKE_BOMB',
+
+  // 燃烧瓶
+  MOLOTOV_COCKTAIL: 'MOLOTOV_COCKTAIL',
 };
 
 const IMAGE_NAMES = {
   CANCEL_THROW_BUTTON: '取消投掷按钮',
+
+  // 投掷物列表-展开按钮
+  EXPAND_MISSILE_BUTTON: '药品列表展开按钮',
+
+  // 投掷物列表-折叠按钮
+  COLLAPSE_MISSILE_BUTTON: '药品列表折叠按钮',
+
 };
 
 /**
@@ -17,9 +31,7 @@ const IMAGE_NAMES = {
 function cancelThrow() {
   const point = mapi.findimage(IMAGE_NAMES.CANCEL_THROW_BUTTON, 0.75, 3, 4, 1, 3);
 
-  const isPointExist = Utils.isPointExist(point);
-
-  if (!isPointExist) {
+  if (Utils.isPointNotExist(point)) {
     return false;
   }
 
