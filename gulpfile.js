7e48188e30ca8571b7895051c29b0ff25476e3a8
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-09 12:07:34 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,5 @@ const insertKeymap = require('./builds/insertKeymap');
 exports.buildScripts = buildScripts;
 exports.buildImages = buildImages;
 exports.insertKeymap = insertKeymap;
+
+// tail -f ~/AppData/Roaming/Sigma-RT/TC\ Games/log/macro_javascript.log
