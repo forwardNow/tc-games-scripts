1bcfe55810d92a74e88d61d2267d22cd90741b76
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-09 12:07:40 +0800

    Update store.js

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -1,65 +1,77 @@
-const KEYS = {
+const state = {
   // 控制 mapi.tip() 是否 启用
-  IS_TIP_ENABLED: 'IS_TIP_ENABLED',
+  isTipEnabled: false,
 
   // 调整过 6 倍镜倍率 的 枪及倍率
-  ADJUSTED_GUNS_OF_6X_SIGHT: 'ADJUSTED_GUNS_OF_6X_SIGHT',
+  adjust6XSightGuns: {}, // { gun: sight, ... }
 
   // 动态调整压枪参数，存储差值
-  DELTA_DELAY: 'DELTA_DELAY',
+  deltaDelayOfGunPressArgs: 0,
 };
 
 
-export default {
-  /**
-   * 获取 是否开启 tip
-   * @return { boolean } 是否开启 tip，true - 启用，false - 禁用
-   */
-  get isTipEnabled() {
-    return mapi.getglobalmap(KEYS.IS_TIP_ENABLED);
-  },
-
-  /**
-   * 设置 是否开启 tip
-   * @param isTipEnabled { boolean } 是否开启 tip，true - 启用，false - 禁用
-   */
-  set isTipEnabled(isTipEnabled) {
-    mapi.setglobalmap(KEYS.IS_TIP_ENABLED, isTipEnabled);
-  },
-
+const getters = {
   /**
    * 获取调整过 6 倍镜倍率的枪的准镜
    * @param gun {string} Gun.CATEGORIES 的值
    * @return {string} Mirror.CATEGORIES 的值
    */
   getMirrorOfAdjustedGun(gun) {
-    const adjustedX6SightsGuns = mapi.getglobalmap(KEYS.ADJUSTED_GUNS_OF_6X_SIGHT) || {};
-
+    const adjustedX6SightsGuns = this.state.adjust6XSightGuns || {}
     return adjustedX6SightsGuns[gun];
-  },
+  }
+};
 
+const mutations = {
   /**
    * 设置调整过 6 倍镜倍率的枪及准镜
    * @param gun {string} Gun.CATEGORIES 的值
    * @param mirror {string} Mirror.CATEGORIES 的值
    */
   setMirrorOfAdjustedGun(gun, mirror) {
-    const adjustedX6SightsGuns = mapi.getglobalmap(KEYS.ADJUSTED_GUNS_OF_6X_SIGHT) || {};
+    const adjustedX6SightsGuns = this.state.adjust6XSightGuns;
 
     adjustedX6SightsGuns[gun] = mirror;
 
-    mapi.setglobalmap(KEYS.ADJUSTED_GUNS_OF_6X_SIGHT, adjustedX6SightsGuns);
+    this.state.adjust6XSightGuns = adjustedX6SightsGuns;
   },
 
-  get deltaDelay() {
-    return mapi.getglobalmap(KEYS.IS_TIP_ENABLED);
-  },
-
-  set deltaDelay(num) {
-    mapi.setglobalmap(KEYS.IS_TIP_ENABLED, num);
-  },
-
-  addDelay(num) {
-    this.deltaDelay = this.deltaDelay + num;
+  addDelayOfGunPressArgs(num) {
+    this.state.deltaDelayOfGunPressArgs = this.state.deltaDelayOfGunPressArgs + num;
   },
 }
+
+const store = {
+  state: {},
+  getters,
+  mutations,
+};
+
+store.state = new Proxy(state, {
+  get(target, propName) {
+    const value = mapi.getglobalmap(propName);
+
+    loginfo(`${propName} = ${value}`);
+
+    if (value == null) {
+      const defaultValue = Reflect.get(target, propName);
+      loginfo(`${propName} = ${defaultValue} (default)`);
+      return defaultValue;
+    }
+
+    return value;
+  },
+
+  set(target, propName, value) {
+    mapi.setglobalmap(propName, value);
+    return true;
+  },
+
+  deleteProperty(target, propName) {
+    mapi.setglobalmap(propName, null);
+    return true;
+  }
+});
+
+
+export default store;
