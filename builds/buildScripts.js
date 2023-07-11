a1769cb88c74c37011956b4369696e15e583c36e
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-11 20:34:14 +0800

    chore builds: buildScripts, 添加 TS 编译

--- builds/buildScripts.js
+++ builds/buildScripts.js
@@ -1,14 +1,25 @@
 const { src, dest, series } = require('gulp');
 const replace = require('gulp-replace');
+const ts = require('gulp-typescript');
 const clean = require('./clean')
-const { CTX_PATH, TCG_SCRIPTS_DIR } = require('./utils');
+const { CTX_PATH, TCG_SCRIPTS_DIR, DIST_PATH } = require('./utils');
+
+const tsProject = ts.createProject('tsconfig.json');
 
 const DEVICE = process.env.DEVICE;
 
 function build() {
-  return src(`src/scripts/*.js`, { cwd: CTX_PATH })
+  return src(['src/scripts/*.js', 'src/scripts/*.ts'], { cwd: CTX_PATH })
+    // 指定编译哪个设备
     .pipe(replace(/\/\*\s*replace\s*start\s*\*\/\s*\w+\s*\/\*\s*replace end\s*\*\//, DEVICE))
-    .pipe(dest(TCG_SCRIPTS_DIR));
+
+    .pipe(tsProject()).js
+
+    // import x from './x' --> import x from 'x'
+    .pipe(replace(/(from\s+['"])(\.\/)(\w+)'/gm, '$1$3'))
+
+    .pipe(dest(TCG_SCRIPTS_DIR))
+    .pipe(dest(DIST_PATH));
 }
 
 module.exports = series(clean(TCG_SCRIPTS_DIR), build);
