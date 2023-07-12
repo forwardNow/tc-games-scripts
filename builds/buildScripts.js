68db12fc66c873c763329aa68be0b717c2970d7c
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-12 20:47:01 +0800

    fix buildScripts: 修复 import ./ 的问题

--- builds/buildScripts.js
+++ builds/buildScripts.js
@@ -8,15 +8,19 @@ const tsProject = ts.createProject('tsconfig.json');
 
 const DEVICE = process.env.DEVICE;
 
+const DEVICE_REGEX = /\/\*\s*replace\s*start\s*\*\/\s*\w+\s*\/\*\s*replace end\s*\*\//;
+const RELATIVE_IMPORT_REGEX = /(from\s+['"])(\.\/)(\w+['"])/gm;
+
 function build() {
   return src(['src/scripts/*.js', 'src/scripts/*.ts'], { cwd: CTX_PATH })
     // 指定编译哪个设备
-    .pipe(replace(/\/\*\s*replace\s*start\s*\*\/\s*\w+\s*\/\*\s*replace end\s*\*\//, DEVICE))
+    // /* replace start */ DEVICE /* replace end */
+    .pipe(replace(DEVICE_REGEX, DEVICE))
 
     .pipe(tsProject()).js
 
     // import x from './x' --> import x from 'x'
-    .pipe(replace(/(from\s+['"])(\.\/)(\w+')/gm, '$1$3'))
+    .pipe(replace(RELATIVE_IMPORT_REGEX, '$1$3'))
 
     .pipe(dest(TCG_SCRIPTS_DIR))
     .pipe(dest(DIST_PATH));
