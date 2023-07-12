178c686db98262f549d4055d64895a2264fa9bd7
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-12 18:08:10 +0800

    feature car: 完成相关功能

--- src/scripts/car.ts
+++ src/scripts/car.ts
@@ -1,16 +1,42 @@
+import { ImageArgs } from '../../types';
+import Utils from './utils';
+import { BIND_KEYS } from './constant';
+
+const IMAGE_ARGS: { [name: string]: ImageArgs } = {
+  GET_OFF_BUTTON: ['下车按钮', 0.75, 4, 3, 4, 2]
+}
+
+const POSITIONS = {
+
+};
+
 /** 是否在车上 */
 function isOnCar() {
+  const point = mapi.findimage(...IMAGE_ARGS.GET_OFF_BUTTON);
 
+  const result = Utils.isPointExist(point);
+
+  return { point, result };
 }
 
 /** 上车 */
 function getOn() {
+  mapi.key(BIND_KEYS.GET_ON_DRIVER);
 
+  return true;
 }
 
 /** 下车 */
 function getOff() {
+  const { result, point } = isOnCar();
 
+  if (!result) {
+    return;
+  }
+
+  mapi.click(point);
+
+  return true;
 }
 
 export default {
