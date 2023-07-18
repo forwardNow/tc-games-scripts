a606ba7d9d540d60c99d37039bcb5d30b09c1234
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-18 19:52:54 +0800

    refactor car: 调整命名风格

--- src/scripts/car.ts
+++ src/scripts/car.ts
@@ -3,16 +3,13 @@ import { utils } from './utils';
 import { BindKeys } from './constant';
 
 export class Car {
-  static IMAGE_ARGS: { [name: string]: ImageArgs } = {
-    GET_OFF_BUTTON: ['下车按钮', 0.75, 4, 3, 4, 2]
+  static ImageArgs: { [name: string]: ImageArgs } = {
+    GetOffButton: ['下车按钮', 0.75, 4, 3, 4, 2]
   }
-
-  static POSITIONS = {
-  };
 
   /** 是否在车上 */
   isOnCar() {
-    const point = utils.findImage(...Car.IMAGE_ARGS.GET_OFF_BUTTON);
+    const point = utils.findImage(...Car.ImageArgs.GetOffButton);
 
     const result = utils.isPointExist(point);
 
